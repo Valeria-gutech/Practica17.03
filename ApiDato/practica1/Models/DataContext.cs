c030987a20b906f996fd9d3cbd956cb259aca639
ValeriaGutierrez <ValeGe@DESKTOP-VESR4FH>
2021-03-17 21:40:31 -0400

    Creando el DataContext

---
+++ ApiDato/practica1/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace practica1.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        public System.Data.Entity.DbSet<practica1.Models.gutierrez> gutierrez { get; set; }
+    }
+}
